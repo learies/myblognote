f1bcff31900137144a6e8529ed3511f50e7ca272
LeAries <learies@ya.ru>
2022-03-05 06:53:33 +0300

    Добавил тест view для post_detail

--- backend/posts/tests/test_views.py
+++ backend/posts/tests/test_views.py
@@ -11,6 +11,9 @@ from posts.tests.data_for_test import (
 INDEX_URL = reverse('posts:index')
 INDEX_TEMPLATE = 'posts/index.html'
 
+POST_DETAIL = 'posts:post_detail'
+POST_DETAIL_TEMPLATE = 'posts/post_detail.html'
+
 
 class PostViewsTests(TestCase):
     @classmethod
@@ -22,6 +25,10 @@ class PostViewsTests(TestCase):
             text=POST_TEXT,
             author=cls.user,
         )
+        cls.POST_DETAIL_URL = reverse(
+            POST_DETAIL,
+            kwargs={'post_id': cls.post.id},
+        )
 
     def setUp(self):
         self.guest_client = Client()
@@ -29,7 +36,8 @@ class PostViewsTests(TestCase):
     def test_pages_uses_correct_template(self):
         """URL-адрес соответствуюет шаблону страницы"""
         templates_pages_names = {
-            INDEX_URL: INDEX_TEMPLATE
+            INDEX_URL: INDEX_TEMPLATE,
+            self.POST_DETAIL_URL: POST_DETAIL_TEMPLATE,
         }
         for url, template in templates_pages_names.items():
             with self.subTest(url=url):
@@ -39,4 +47,9 @@ class PostViewsTests(TestCase):
     def test_post_appear_in_page_index(self):
         """Тест отображения поста на главной странице"""
         response = self.guest_client.get(INDEX_URL)
+        self.assertContains(response, self.post)
+
+    def test_post_detail_appear_in_page_index(self):
+        """Тест отображения поста на главной странице"""
+        response = self.guest_client.get(self.POST_DETAIL_URL)
         self.assertContains(response, self.post)
