a1b9c3b75c991e4e70a8484c2c3f31c79f0fd49d
LeAries <learies@ya.ru>
2022-03-05 22:32:24 +0300

    Добавил тесты для profile

--- backend/posts/tests/test_urls.py
+++ backend/posts/tests/test_urls.py
@@ -13,6 +13,7 @@ INDEX_URL = '/'
 INDEX_TEMPLATE = 'posts/index.html'
 
 POST_DETAIL_TEMPLATE = 'posts/post_detail.html'
+PROFILE_TEMPLATE = 'posts/profile.html'
 
 
 class StaticURLTests(TestCase):
@@ -26,6 +27,7 @@ class StaticURLTests(TestCase):
             author=cls.user,
         )
         cls.POST_DETAIL_URL = f'/post/{cls.post.id}/'
+        cls.PROFILE_URL = f'/profile/{cls.user}/'
 
     def setUp(self):
         self.guest_client = Client()
@@ -40,11 +42,17 @@ class StaticURLTests(TestCase):
         response = self.guest_client.get(self.POST_DETAIL_URL)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    def test_profile_status_page(self):
+        """Доступность profile страницы"""
+        response = self.guest_client.get(self.PROFILE_URL)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+    
     def test_urls_uses_correct_template(self):
         """URL-адрес соотвествует шаблону страницы"""
         templates_url_names = {
             INDEX_URL: INDEX_TEMPLATE,
             self.POST_DETAIL_URL: POST_DETAIL_TEMPLATE,
+            self.PROFILE_URL: PROFILE_TEMPLATE,
         }
         for url, template in templates_url_names.items():
             with self.subTest(url=url):
--- backend/posts/tests/test_views.py
+++ backend/posts/tests/test_views.py
@@ -14,6 +14,9 @@ INDEX_TEMPLATE = 'posts/index.html'
 POST_DETAIL = 'posts:post_detail'
 POST_DETAIL_TEMPLATE = 'posts/post_detail.html'
 
+PROFALE = 'posts:profile'
+PROFILE_TEMPLATE = 'posts/profile.html'
+
 
 class PostViewsTests(TestCase):
     @classmethod
@@ -29,6 +32,10 @@ class PostViewsTests(TestCase):
             POST_DETAIL,
             kwargs={'post_id': cls.post.id},
         )
+        cls.PROFILE_URL = reverse(
+            PROFALE,
+            kwargs={'username': cls.user},
+        )
 
     def setUp(self):
         self.guest_client = Client()
@@ -38,6 +45,7 @@ class PostViewsTests(TestCase):
         templates_pages_names = {
             INDEX_URL: INDEX_TEMPLATE,
             self.POST_DETAIL_URL: POST_DETAIL_TEMPLATE,
+            self.PROFILE_URL: PROFILE_TEMPLATE,
         }
         for url, template in templates_pages_names.items():
             with self.subTest(url=url):
@@ -52,4 +60,9 @@ class PostViewsTests(TestCase):
     def test_post_detail_appear_in_page_index(self):
         """Тест отображения поста на главной странице"""
         response = self.guest_client.get(self.POST_DETAIL_URL)
+        self.assertContains(response, self.post)
+
+    def test_profile_page(self):
+        """Страница профиля пользователя"""
+        response = self.guest_client.get(self.PROFILE_URL)
         self.assertContains(response, self.post)
