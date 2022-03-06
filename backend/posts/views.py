f039513981e7f07ebac212b5c79dc0e3e3e8bb41
LeAries <learies@ya.ru>
2022-03-06 10:59:55 +0300

    Поправил шаблоны страниц

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -24,6 +24,7 @@ def profile(request, username):
     author = get_object_or_404(User, username=username)
     posts = Post.objects.filter(author=author)
     context = {
+        'author': author,
         'posts': posts,
     }
     return render(request, 'posts/profile.html', context)
