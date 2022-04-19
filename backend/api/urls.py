54971e8a14dbba9206eb3a3c6eca7a5a026a3e48
LeAries <learies@ya.ru>
2022-04-19 07:54:49 +0300

    Подключил JWT-аутентификацию

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,6 +8,6 @@ router_v1 = DefaultRouter()
 router_v1.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = [
-    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/auth/', include('djoser.urls.jwt')),
     path('v1/', include(router_v1.urls)),
 ]
