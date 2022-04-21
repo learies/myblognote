feed4ce394da67b183a76bd7292fb29da53aa1b0
LeAries <learies@ya.ru>
2022-04-21 14:23:49 +0300

    Добавил вывод, имя автора и название группы

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.relations import SlugRelatedField
+from rest_framework.serializers import ModelSerializer, StringRelatedField
 
 from posts.models import Post
 
-
 class PostSerializer(ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+    group = StringRelatedField(read_only=True)
 
     class Meta:
         model = Post
